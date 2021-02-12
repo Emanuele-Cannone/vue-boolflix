3ceb8befc210255eea5dc63e144d98ddb78a01ca
Emanuele <cannone.emanuele@gmail.com>
2021-02-12 13:10:01 +0100

    header css + js ok

--- vue/script.js
+++ vue/script.js
@@ -1,50 +1,4 @@
-
-
-// campione di risposta FILM:
-    // data.results:
-        // {
-        //     "adult": false,
-        //     "backdrop_path": null,
-        //     "genre_ids": [],
-        //     "id": 391889,
-        //     "original_language": "en",
-        //     "original_title": "Bella ciao",
-        //     "overview": "A documentary film about the violent riots during the 2001 G8 meeting in Genoa.",
-        //     "popularity": 9.09,
-        //     "poster_path": null,
-        //     "release_date": "2002-04-08",
-        //     "title": "Bella ciao",
-        //     "video": false,
-        //     "vote_average": 5.5,
-        //     "vote_count": 1
-        // aggiunto nel map "votazione" : 3
-        // }
-
-
-// campione di risposta SERIE-TV:
-    // {
-    //     "backdrop_path": "/xGexTKCJDkl12dTW4YCBDXWb1AD.jpg",
-    //     "first_air_date": "2017-05-02",
-    //     "genre_ids": [
-    //           80,
-    //           18
-    //          ],
-    //      "id": 71446,
-    //      "name": "Money Heist",
-    //      "origin_country": [
-    //          "ES"
-    //         ],
-    //     "original_language": "es",
-    //     "original_name": "La casa de papel",
-    //     "overview": "To carry out the biggest heist in history, a mysterious man called The Professor recruits a band of eight robbers who have a single characteristic: none of them has anything to lose. Five months of seclusion - memorizing every step, every detail, every probability - culminate in eleven days locked up in the National Coinage and Stamp Factory of Spain, surrounded by police forces and with dozens of hostages in their power, to find out whether their suicide wager will lead to everything or nothing.",
-    //     "popularity": 217.541,
-    //     "poster_path": "/MoEKaPFHABtA1xKoOteirGaHl1.jpg",
-    //     "vote_average": 8.3,
-    //     "vote_count": 12143
-    // }
-
-
-var app = new Vue({
+ var app = new Vue({
     el: '#contenitore',
     data:{
         ricerca: '',
@@ -52,7 +6,23 @@ var app = new Vue({
         tipo: 'all',
         arrayRisultato: [],
         arrayAll: [],
-        
+        arrayScelta: [
+            {
+                tipologia: 'all',
+                scelta: 'Home',
+                nonSoComeCHiamarlo: true
+            },
+            {
+                tipologia: 'tv',
+                scelta: 'Serie Tv',
+                nonSoComeCHiamarlo: false
+            },
+            {
+                tipologia: 'movie',
+                scelta: 'Film',
+                nonSoComeCHiamarlo: false
+            },
+        ]
     },
     methods: {
 
@@ -161,6 +131,28 @@ var app = new Vue({
 
 
 
+        },
+
+        scelta(element, index){
+            
+            // per ogni elemento
+            this.arrayScelta.forEach(element => {
+                
+                // se il booleano è vero
+                if (element.nonSoComeCHiamarlo == true) {
+                    
+                    // salvalo nella variabile
+                    element.nonSoComeCHiamarlo = this.elementoVisualizzato
+
+                }
+
+            });
+
+            this.tipo = element;
+
+            // cambia il valore del booleano da vero a falso e viceversa
+            this.arrayScelta[index].nonSoComeCHiamarlo = !this.arrayScelta[index].nonSoComeCHiamarlo;
+        
         }
 
 
@@ -169,7 +161,3 @@ var app = new Vue({
 
 });
 
-
-
-// tv?api_key=17afad9915b223d4e647b46ea79354ef&query=casa
-// movie?api_key=17afad9915b223d4e647b46ea79354ef&query=Casa de los Babys
